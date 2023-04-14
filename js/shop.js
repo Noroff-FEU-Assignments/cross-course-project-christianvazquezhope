403323d067638fc5f54f1c8de0f33e7e523b161a
Christian Vazquez Hope <christian@digithings.no>
2023-04-14 20:32:24 +0200

    Added shop js

---
+++ js/shop.js
@@ -0,0 +1,4 @@
+const apiUrl = `https://dev.digithings.io/wp-api/wp-json/wc/v3/products?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}`;
+const consumerKey = 'ck_70d95adcb2274b06188009375dc41c42fd241a73';
+const consumerSecret = 'cs_9fb15a5565510f5e4e1908255c4744829c872668';
+
